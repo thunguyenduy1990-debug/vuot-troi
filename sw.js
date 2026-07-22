51cdb58d6bd3d1c4983b8dbfd3d0c61d5eb2df05
thunguyenduy1990-debug <thu.nguyenduy1990@gmail.com>
2026-07-22 10:06:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="vuottroi-v10"; // MỖI LẦN UPDATE LỚN: đổi số v2->v3->... để ép xóa cache cũ
+const CACHE="vuottroi-v12"; // MỖI LẦN UPDATE LỚN: đổi số v2->v3->... để ép xóa cache cũ
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 
 self.addEventListener("install",e=>{
